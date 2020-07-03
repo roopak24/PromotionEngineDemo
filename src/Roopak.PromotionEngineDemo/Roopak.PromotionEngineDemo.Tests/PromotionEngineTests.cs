e201f13cbbea309e2ab4e3b570ce7409d038bf58
B. Roopak Rao <roopak24@gmail.com>
2020-07-03 14:49:51 +0530

    Initial setup

---
+++ src/Roopak.PromotionEngineDemo/Roopak.PromotionEngineDemo.Tests/PromotionEngineTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Roopak.PromotionEngineDemo.Tests
+{
+    public class PromotionEngineTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
